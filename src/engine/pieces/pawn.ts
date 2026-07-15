0bae39ffc1040a3bf8535d856d0059dcd1291c0f
fabrizzioj <fabrizzio.jiglau@softwire.com>
2026-07-15 12:29:50 +0300

    solved r1: w and b pawns

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,13 +1,16 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board){
+        let row = (this.player == 0) ? board.findPiece(this).row + 1 : board.findPiece(this).row - 1;
+        let col = board.findPiece(this).col;
+        return Square.at(row, board.findPiece(this).col);
     }
 }
