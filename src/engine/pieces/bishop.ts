52cb4017c171e25ce704f96c35c12119ac8ab039
fabrizzioj <fabrizzio.jiglau@softwire.com>
2026-07-15 15:20:40 +0300

    solved r4: bishop movement

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,13 +1,32 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board){
+        let row = board.findPiece(this).row
+        let col = board.findPiece(this).col
+        let moves = [];
+
+        for (let i = -7; i <= 7; i++) {
+            let newRow = row + i;
+            let newCol = col + i;
+            if (newRow >= 0  && newRow <= 7 && newCol >= 0 && newCol <= 7 && i != 0 )
+                moves.push(Square.at(newRow, newCol));
+        }
+
+        for (let i = -7; i <= 7; i++) {
+            let newRow = row + i;
+            let newCol = col - i;
+            if (newRow >= 0  && newRow <= 7 && newCol >= 0 && newCol <= 7 && i != 0 )
+                moves.push(Square.at(newRow, newCol));
+        }
+
+        return moves;
     }
 }
