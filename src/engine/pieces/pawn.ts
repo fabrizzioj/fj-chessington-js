3af529e32f7a0594ab5dbff09c5e5372b741a9cd
fabrizzioj <fabrizzio.jiglau@softwire.com>
2026-07-15 16:50:00 +0300

    solved r8 + pawn code refactor

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -8,35 +8,46 @@ export default class Pawn extends Piece {
         super(player);
     }
 
-    public getAvailableMoves(board: Board){
-        switch(this.player){
-            case 0:
-                let wrow1 = board.findPiece(this).row + 1;
-                let wrow2 = board.findPiece(this).row + 2;
-                let wcol = board.findPiece(this).col;
+    public getAvailableMoves(board: Board) {
+        let currentSquare = board.findPiece(this);
+        let row = currentSquare.row;
+        let col = currentSquare.col;
+        let moves = [];
 
-                let wmove1 = Square.at(wrow1, wcol);
-                let wmove2 = Square.at(wrow2, wcol);
+        switch (this.player) {
+            case 0: // White
+                let wMove1 = Square.at(row + 1, col);
 
-                if(board.findPiece(this).row > 1)
-                    return [wmove1]
-                else if(board.findPiece(this).row == 1)
-                    return [wmove1,wmove2]
+                if (board.getPiece(wMove1) === undefined) {
+                    moves.push(wMove1);
 
+                    if (row == 1) {
+                        let wMove2 = Square.at(row + 2, col);
+
+                        if (board.getPiece(wMove2) === undefined) {
+                            moves.push(wMove2);
+                        }
+                    }
+                }
                 break;
-            default:
-                let brow1 = board.findPiece(this).row - 1;
-                let brow2 = board.findPiece(this).row - 2;
-                let bcol = board.findPiece(this).col;
 
-                let bmove1 = Square.at(brow1, bcol);
-                let bmove2 = Square.at(brow2, bcol);
+            default: // Black
+                let bMove1 = Square.at(row - 1, col);
 
-                if(board.findPiece(this).row < 6)
-                    return [bmove1]
-                else if(board.findPiece(this).row == 6)
-                    return [bmove1,bmove2]
+                if (board.getPiece(bMove1) === undefined) {
+                    moves.push(bMove1);
+
+                    if (row == 6) {
+                        let bMove2 = Square.at(row - 2, col);
+
+                        if (board.getPiece(bMove2) === undefined) {
+                            moves.push(bMove2);
+                        }
+                    }
+                }
                 break;
         }
+
+        return moves;
     }
 }
--- tests/engine/pieces/pawn.spec.ts
+++ tests/engine/pieces/pawn.spec.ts
@@ -37,7 +37,7 @@ describe('Pawn', () => {
 
     describe('black pawns', () => {
 
-        let board:Board;
+        let board: Board;
         beforeEach(() => board = new Board(Player.BLACK));
 
         it('can only move one square down if they have already moved', () => {
@@ -62,27 +62,27 @@ describe('Pawn', () => {
         });
 
 
-    it('cannot move if there is a piece in front', () => {
-        const pawn = new Pawn(Player.BLACK);
-        const blockingPiece = new Rook(Player.WHITE);
-        board.setPiece(Square.at(6, 3), pawn);
-        board.setPiece(Square.at(5, 3), blockingPiece);
+        it('cannot move if there is a piece in front', () => {
+            const pawn = new Pawn(Player.BLACK);
+            const blockingPiece = new Rook(Player.WHITE);
+            board.setPiece(Square.at(6, 3), pawn);
+            board.setPiece(Square.at(5, 3), blockingPiece);
 
-        const moves = pawn.getAvailableMoves(board)!;
+            const moves = pawn.getAvailableMoves(board)!;
 
-        moves.should.be.empty;
-    });
-
-    it('cannot move two squares if there is a piece two sqaures in front', () => {
-        const pawn = new Pawn(Player.BLACK);
-        const blockingPiece = new Rook(Player.WHITE);
-        board.setPiece(Square.at(6, 3), pawn);
-        board.setPiece(Square.at(4, 3), blockingPiece);
-
-        const moves = pawn.getAvailableMoves(board)!;
-
-        moves.should.not.deep.include(Square.at(4, 3));
-    });
-
+            moves.should.be.empty;
         });
+
+        it('cannot move two squares if there is a piece two sqaures in front', () => {
+            const pawn = new Pawn(Player.BLACK);
+            const blockingPiece = new Rook(Player.WHITE);
+            board.setPiece(Square.at(6, 3), pawn);
+            board.setPiece(Square.at(4, 3), blockingPiece);
+
+            const moves = pawn.getAvailableMoves(board)!;
+
+            moves.should.not.deep.include(Square.at(4, 3));
+        });
+
     });
+});
